e9d80d0a0e76679978534420d2bb4b284941ba64
fran1259 <71811335+fran1259@users.noreply.github.com>
2023-02-07 21:00:11 +0100

    queda poner x:Name's y pestaña 'Juego'

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,45 @@ namespace Diccionario_DDi
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private Dictionary<String, String> palabrasIngles_Espanol = null;
+        private Dictionary<String, String> palabrasEspanol_Ingles = null;
+
         public MainWindow()
         {
             InitializeComponent();
+            crearDiccionario();
+        }
+
+        private void crearDiccionario()
+        {
+            palabrasEspanol_Ingles = new Dictionary<string, string>() {
+                {"Negro","Black"},{"Gato","Cat"},{"Perro","Dog"},{"Casa","House"},{"Blanco","White"},
+                {"Naranja","Orange"},{"Flor","Flower"},{"Ventana","Window"},{"Ordenador","Computer"},{"Verde","Green"}
+            };
+            palabrasIngles_Espanol = new Dictionary<string, string>();
+            foreach(var clave in palabrasEspanol_Ingles)
+            {
+                if (!palabrasIngles_Espanol.ContainsKey(clave.Value))
+                {
+                    palabrasIngles_Espanol.Add(clave.Value, clave.Key);
+                }
+            }
+        }
+
+        private void traducirPalabras(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cambiaVentana2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cambiaVentana1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
