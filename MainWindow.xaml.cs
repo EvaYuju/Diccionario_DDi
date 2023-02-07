dfb8552a02f5a54a6ab50f84982d1a5af906b904
EvaYuju <elopezmarquez@safareyes.es>
2023-02-07 23:16:06 +0100

    Pestaña ingles-español OK

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Diccionario_DDi.Traductor;
 
 namespace Diccionario_DDi
 {
@@ -19,10 +20,34 @@ namespace Diccionario_DDi
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {   // Inicializamos la clase traductor
+        TraductorIE ob = new TraductorIE();
+       
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {   // metodo que vacia el campo de escritura
+            txtBoxEscribir.Text = "";
+        }
+
+        // Método para el boton de traducir
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {   // Llamamos a nuestro objeto.y llamamos a la propiedad entrada=valor del textBox
+            ob.Entrada = txtBoxEscribir.Text;
+            labelTraduccion.Content = ob.Traduceme(); // el control de traduccion(label) Llame al método y lo muestre
+
+        }
+
+        // Método para el boton de borrar 
+        private void btnBorrar_Click(object sender, RoutedEventArgs e)
+        {
+            // Llamamos a los controles
+            txtBoxEscribir.Text = string.Empty;
+            labelTraduccion.Content = string.Empty;
+
         }
     }
 }
---
+++ Traductor/TraductorIE.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diccionario_DDi.Traductor
+{
+    class TraductorIE
+    {
+        string entrada;
+        string salida;
+
+        public string Entrada { get => entrada; set => entrada = value; }
+        public string Salida { get => salida; set => salida = value; }
+
+        public string[] español = { "agua", "fuego", "amor", "aire", "sol", "luna", "evaluar", "rabia","disminuir", "enfado","voluntad", "informe", "ubicación", "etiqueta", "personalizar", "diagnosticando", "manteniendo", "instalamdo", "cliente", "lanzado", "proporcionar", "suministros", "horrible" };
+        public string[] inglés = { "water", "fire", "love", "air", "sun", "moon", "assess", "rage", "diminish", "anger", "willingness", "report", "location", "tag", "customize", "diagnosing", "maintaining", "installing", "client", "launched", "provide", "supplies", "ghastly" };
+
+
+        public string Traduceme()
+        {
+            bool encontrar = false;
+            int cont = 0;
+            while (encontrar == false)
+            {
+                if (entrada == inglés[cont])
+                {
+                    salida = español[cont];
+                    encontrar = true;
+                }
+                cont++;
+            }
+            return salida;
+        }
+    }
+}
